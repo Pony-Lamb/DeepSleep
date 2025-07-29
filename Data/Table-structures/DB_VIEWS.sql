bb3165d2be5fba0b1fdbaf00804c54bc5afe714d
kiki <840084384@qq.com>
2025-07-30 01:00:50 +0800

    Add semicolon to views SQL

--- Data/Table-structures/DB_VIEWS.sql
+++ Data/Table-structures/DB_VIEWS.sql
@@ -16,7 +16,7 @@ ON A.asset_id = B.asset_id
 WHERE data_date = (SELECT MAX(data_date)
 				   FROM assets C
                    WHERE C.asset_id = b.asset_id
-                   GROUP BY C.asset_id)
+                   GROUP BY C.asset_id);
                    
 				   
 #视图-投资组合资产市值表，展示用户各投资组合的市值
@@ -29,7 +29,7 @@ LEFT JOIN user_info B
 ON A.user_id = B.user_id
 LEFT JOIN assets C
 ON A.asset_id = C.asset_id
-GROUP BY B.user_name, A.portfolio_name, data_date
+GROUP BY B.user_name, A.portfolio_name, data_date;
 
 #视图-全表，仅查询数据结构，按需使用
 SELECT *
@@ -37,12 +37,12 @@ FROM portfolio A
 LEFT JOIN user_info B
 ON A.user_id = B.user_id
 LEFT JOIN assets C
-ON A.asset_id = C.asset_id
+ON A.asset_id = C.asset_id;
 
 #视图-用户投资组合表，返回各用户持有的投资组合
 SELECT DISTINCT A.user_id,
 	   A.user_name,
 	   B.portfolio_name
 FROM user_info A, portfolio B
-WHERE A.user_id = B.user_id
+WHERE A.user_id = B.user_id;
 
