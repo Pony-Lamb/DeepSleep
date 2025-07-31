1b7cf692b00fea3a0fbdb45da7f1006535a9d2c1
bob <bobzzp@126.com>
2025-07-31 10:49:58 +0800

    Bob
    更新代码结构

--- Data/data-query/query_asset_value.sql
+++ Data/data-query/query_asset_value.sql
@@ -8,7 +8,7 @@ FROM full_asset_value
 #AND portfolio_name = ?
 GROUP BY user_name, portfolio_name;
 
-#查询-查用户名下，各投资组合下各类资产市值，需要给到日期，用户姓名、投资组合名（可选）
+#查询-饼状图各类资产市值，需要给到日期（必须），用户姓名、投资组合名（可选）
 SELECT user_name,
 	   portfolio_name,
 	   category,
